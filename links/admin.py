13c762ab4341f2b9d5211ccb75ee2a2e051b75e5
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-11-10 14:31:48 +0300

    register model

--- links/admin.py
+++ links/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from links.models import Link
+
+
+@admin.register(Link)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_url', 'short_url', 'created_at')
+    list_filter = ('id', 'created_at')
+    search_fields = ('id',)
+    ordering = ('-created_at',)
