32d834d097abc86d1341412107d3c11266e946ee
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-11-10 20:06:22 +0300

    configure urls

--- links/urls.py
+++ links/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from links.apps import LinksConfig
-from links.views import LinkListView, LinkCreateView
+from links.views import LinkListView, LinkCreateView, LinkRedirectView
 
 app_name = LinksConfig.name
 
 urlpatterns = [
-    path('', LinkListView.as_view(), name='link_list'),
-    path('create/', LinkCreateView.as_view(), name='link_create')
+    path('links', LinkListView.as_view(), name='link_list'),
+    path('links/create/', LinkCreateView.as_view(), name='link_create'),
+    path('<str:short_url>', LinkRedirectView.as_view(), name='link_redirect'),
 ]
